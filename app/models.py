99f0c74f86eeffdb271b4be03c9a7f6a5092fe3f
vishalipar <vishalipar04@gmail.com>
2024-07-29 22:59:10 +0530

    Developed registration, login and profile page

--- app/models.py
+++ app/models.py
@@ -1,6 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+STATE_CHOICES = (
+    ('Andaman & Nicobar Islands','Andaman & Nicobar Islands'),
+    ('Andra pradesh', 'Andra pradesh'),
+    ('Assam','Assam'),
+    ('Arunachal Pradesh','Arunachal Pradesh'),
+    ('Bihar','Bihar'),
+    ('Chandigarh','Chandigarh'),
+    ('Chattisgarh','Chattisgarh'),
+    ('Dadra & Nagar Haweli','Dadra & Nagar Haweli'),
+    ('Daman & Diu','Daman & Diu'),
+    ('Delhi','Delhi'),
+    ('Goa','Goa'),
+    ('Gujrat','Gujrat'),
+    ('Haryana','Haryana'),
+    ('Himachal Pradesh','Himachal Pradesh'),
+    ('Maharashtra','Maharashtra'),
+)
+
 
 CATEGORY_CHOICES = (
     ('CR','Curd'),
@@ -25,3 +44,17 @@ class Product(models.Model):
     
     def __str__(self):
         return self.title
+    
+    
+    
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=100)
+    
+    def __str__(self):
+        return self.name
--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
 from . import views
+from django.contrib.auth import views as auth_view
+from .forms import LoginForm,MyPasswordResetForm
 
 urlpatterns = [
     path('', views.home),
@@ -9,6 +11,10 @@ urlpatterns = [
     path('category/<slug:val>/', views.CategoryView.as_view(), name='category'),
     path('category-title/<val>/', views.CategoryTitle.as_view(), name='category-title'),
     path('product-detail/<int:pk>/', views.ProductDetail.as_view(), name='product-detail'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
     
     path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
+    path('accounts/login/', auth_view.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
+    path('password-reset/', auth_view.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm), name='password_reset'),
+    
 ]
