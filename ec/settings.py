de8e549d9613f028ace597855f89ff31bb08c184
Vishal Ipar <153973262+vishalipar@users.noreply.github.com>
2024-08-03 19:31:38 +0530

    Update settings.py

--- ec/settings.py
+++ ec/settings.py
@@ -115,10 +115,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-import os
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = BASE_DIR/'static'
 
 # STATIC_ROOT = BASE_DIR/'static'
 
