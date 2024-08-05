56ee5984398cf32a782d0e38f33f5cbe10b5d57b
Vishal Ipar <153973262+vishalipar@users.noreply.github.com>
2024-08-05 08:47:50 +0530

    Update settings.py

--- ec/settings.py
+++ ec/settings.py
@@ -123,12 +123,9 @@ USE_TZ = True
 
 import os
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-=======
-STATIC_ROOT = BASE_DIR/'static'
-
-# STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
